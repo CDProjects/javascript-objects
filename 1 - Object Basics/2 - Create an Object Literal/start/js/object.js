80d61090124e552d2f407411c81fa514c3b802ff
Kiwicase <caseydentprojects@gmail.com>
2023-04-12 10:01:33 +0300

    Added to message variable to access object

--- 1 - Object Basics/2 - Create an Object Literal/start/js/object.js
+++ 1 - Object Basics/2 - Create an Object Literal/start/js/object.js
@@ -6,5 +6,5 @@ const person = {
     skills: ['JavaScript', 'HTML', 'CSS']
 };
 
-const message = `Hi, I'm ${person.name}.`;
+const message = `Hi, I'm ${person.name}. I live in ${person.city}.`;
 console.log(message);
