90dc1da2ef105fa0d9de49f826d7970f0fe1ca4d
cdprojects <caseydentprojects@gmail.com>
2023-08-16 11:27:24 +0300

    Created Pets Object

--- 2 - Loop Through Objects/4 - Build an Object Challenge/start/js/pets.js
+++ 2 - Loop Through Objects/4 - Build an Object Challenge/start/js/pets.js
@@ -3,3 +3,41 @@
   Each object should have the following properties: 
   name, type, breed, age, and photo
 */
+
+const pets = [
+  {
+    name: 'Buddy',
+    type: 'Dog',
+    breed: 'Golden Retriever',
+    age: 5,
+    photo: 'img/golden.jpg'
+  },
+  {
+    name: 'Whiskers',
+    type: 'Cat',
+    breed: 'Tabby',
+    age: 3,
+    photo: 'img/tabby.jpg'
+  },
+  {
+    name: 'Charlie',
+    type: 'Dog',
+    breed: 'Dachshund',
+    age: 1,
+    photo: 'img/dachshund.jpg'
+  },
+  {
+    name: 'Mohammed',
+    type: 'Cat',
+    breed: 'Persian',
+    age: 2,
+    photo: 'img/persian.jpg'
+  },
+  {
+    name: 'Karrrnt',
+    type: 'Dog',
+    breed: 'Aussie',
+    age: 4,
+    photo: 'img/aussie.jpg',
+  }
+];
